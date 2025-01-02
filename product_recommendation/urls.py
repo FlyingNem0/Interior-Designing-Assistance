1cdb6a6f93bd5f8a25c1e61a0ee6864e6c262608
FlyingNem0 <jibinvincent520@gmail.com>
2025-01-02 22:54:01 +0530

    jibin

--- product_recommendation/urls.py
+++ product_recommendation/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('online_recommendation/', views.online_recommendation, name='onlin_recommendation'),
+    path('online_recommendation/', views.online_recommendation, name='online_recommendation'),
     path('offline_recommendation/', views.offline_recommendation, name='offline_recommendation'),
     #path('login/', views.login, name='login'),
     #path('register/', views.register, name='register'),
